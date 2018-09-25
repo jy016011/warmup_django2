096965c38e6e2524fd180ef253d7da1a8e11e924
glad <glad1470@naver.com>
2018-09-25 12:05:17 +0000

    final ver

--- temp.py
+++ temp.py
@@ -2,24 +2,40 @@ import serial
 import urllib.request
 serialFromArduino = serial.Serial("/dev/ttyACM0",9600)
 serialFromArduino.flushInput()
-status = 'empty'
-count = 0
-seat_num = 1
+count = 0 # seat timer
+seat_status = False
+
 while True:
     distance = serialFromArduino.readline()
     print(distance)
-    if status == 'empty':
-        if int(distance) <= 70:
-            status = 'seated'
-            data_b = (str(seat_num) + "\r\n" + status + "\r\n" + str(distance)).encode('utf-8')
-            req = urllib.request.Request(url='http://192.168.0.180', data= data_b ,method='PUT')
-        else:
-            count += 1
+    if int(distance) <= 70: # if seat is not empty
+        if not seat_status:
+            seat_status = True
+            status = {'num': 1 , 'status': 'seated'}
+            data_en =urllib.parse.urlencode(status).encode('utf-8')
+            req = urllib.request.Request(url='http://192.168.0.247:8080/show/seat',data = data_en,method='PUT')
+            with urllib.request.urlopen(req) as f:
+                pass
+            print(f.status)
+            print(f.reason)
+        elif status['status'] == 'empty':
+            status = {'num': 1 , 'status': 'seated'}
+            data_en =urllib.parse.urlencode(status).encode('utf-8')
+            req = urllib.request.Request(url='http://192.168.0.247:8080/show/seat',data = data_en,method='PUT')
+            with urllib.request.urlopen(req) as f:
+                pass
+            print(f.status)
+            print(f.reason)
     else:
+        count += 1
+        print(count)
         if count >= 5:
-            status = 'empty'
-            data_b = str(seat_num) + "\r\n" + status + "\r\n" + str(distance).encode('utf-8')
-            req = urllib.request.Request(url='http://192.168.0.180', data= data_b ,method='PUT')
+            seat_status = False
+            status = {'num': 1 , 'status': 'empty'}
+            data_en =urllib.parse.urlencode(status).encode('utf-8')
+            req = urllib.request.Request(url='http://192.168.0.247:8080/show/seat',data = data_en,method='PUT')
+            with urllib.request.urlopen(req) as f:
+                pass
+            print(f.status)
+            print(f.reason)
             count = 0
-    
-    
