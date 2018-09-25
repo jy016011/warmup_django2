771f552a5302bcb1ca9ab34ff9b7def6003369d1
jy016011 <jy016011@naver.com>
2018-09-25 23:13:08 +0900

    subs

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 class Seats(models.Model):
-	is_seat_empty = models.CharField(max_length=200)
-	seat_num = models.IntegerField(default=0)
+	is_seat_empty = models.CharField(max_length=200) #seat's status
+	seat_num = models.IntegerField(default=0)# seat number
 
 # Create your models here.
--- blog/views.py
+++ blog/views.py
@@ -11,24 +11,24 @@ def show_seat(request):
 
 	seats = Seats.objects.all()
 	str1 = ''
-	if request.method == "PUT":
-		decoded_data = request.read().decode('utf-8')
-		http_bas = "http://django/django?"
+	if request.method == "PUT": #requet type is PUT
+		decoded_data = request.read().decode('utf-8')#decode by UTF-8
+		http_bas = "http://django/django?"#to make http from url
 		http_bas += decoded_data
-		parsed = urlparse.urlparse(http_bas)
+		parsed = urlparse.urlparse(http_bas) #parsed from url
 		#print(parsed.query)
-		seat_num = urlparse.parse_qs(parsed.query)['num'][0]
-		seat_status = urlparse.parse_qs(parsed.query)['status'][0]
+		seat_num = urlparse.parse_qs(parsed.query)['num'][0]#parsed.query is dict type and seat_num = parsed.query['num'] (key value is 'num')
+		seat_status = urlparse.parse_qs(parsed.query)['status'][0]# seat_status = parsed.query['status'] (key value is 'status')
 		print('seat num',seat_num , 'seat status',seat_status)
-		for seat in seats:
-			if(seat.seat_num == int(seat_num)):
+		for seat in seats: # seats objects
+			if(seat.seat_num == int(seat_num)): 
 				seat.is_seat_empty = seat_status
 				print(seat.is_seat_empty)
-			seat.save()
+			seat.save() #commit db changed 
 		
-	context = {'seats' : seats}
+	context = {'seats' : seats} #in template 'seats' has seats object's value
 	
-	return render(request, 'blog/show_seat.html',context)
+	return render(request, 'blog/show_seat.html',context) #return response
 
 
 # Create your views here.
