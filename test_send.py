ef3ed01f3cbbf60ebdbb9b406868cec46dea75b3
jy016011 <jy016011@naver.com>
2018-09-25 21:29:24 +0900

    html

--- test_send.py
+++ test_send.py
@@ -1,6 +1,6 @@
 import urllib.request
 
-status = {'num': 2 , 'status': 'seated'}
+status = {'num': 3 , 'status': 'seated'}
 data_en =urllib.parse.urlencode(status).encode('utf-8')
 req = urllib.request.Request(url='http://127.0.0.1:8000/show/seat',data = data_en,method='PUT')
 with urllib.request.urlopen(req) as f:
